5c5b56da2a50d4053e290466248776dfd2317b54
steve hanlon <rshanlon@gmail.com>
2019-02-07 23:12:32 -0800

    Added methods for adding numbers and operators to view screen; added method to clear view screen; added basic array of keys to be expanded to an array of objects - an object for each calculator key

--- src/App.js
+++ src/App.js
@@ -1,38 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+const keyboard = ["MC", "M+", "&#247;", "&#215;", "7", "8", "9", "&#8722;", "4", "5", "6", "&#43;", "1", "2", "3", "&#61;", "0", "."]
+
+class Calculator extends React.Component {
+  state = {
+    total: 0,
+    equation: "",
+  }
+
+  clearScreen = () => {
+    this.setState({
+      total: 0,
+      equation: "",
+    });
+  }
+
+  enterEquation = (event) => {
+    this.setState({
+      total: event.target.value,
+      equation: this.state.equation += event.target.value
+    });
+  }
+
+  equationEquals = () => {
+    let solution = eval(this.state.equation);
+    this.setState({ total: solution });
+  }
+
   render() {
     return (
-      <div className="App">
-        <main id="calc-body">
-          <section id="calc-section">
-            <input id="view-screen" type="number" />
-          </section>
-          <section id="calc-btn-section">
-            <button className="black-btn" id="mc">MC</button>
-            <button className="black-btn" id="mplus">M+</button>
-            <button className="black-btn" id="div-sign">&#247;</button>
-            <button className="black-btn" id="x-sign">&#215;</button>
-            <button className="gray-btn" id="7">7</button>
-            <button className="gray-btn" id="8">8</button>
-            <button className="gray-btn" id="9">9</button>
-            <button className="gray-btn" id="minus">&#8722;</button>
-            <button className="gray-btn" id="4">4</button>
-            <button className="gray-btn" id="5">5</button>
-            <button className="gray-btn" id="6">6</button>
-            <button className="gray-btn" id="plus">&#43;</button>
-            <button className="gray-btn" id="1">1</button>
-            <button className="gray-btn" id="2">2</button>
-            <button className="gray-btn" id="3">3</button>
-            <button className="red-btn" id="equals">&#61;</button>
-            <button className="gray-btn" id="zero">0</button>
-            <button className="gray-btn" id="decimal">.</button>
-          </section>
-        </main>
-      </div>
-    );
+      <main id="calc-body">
+        <section id="view-screen-container">
+          <input id="view-screen"
+                 type="text"
+                 value={this.state.total}
+                 pattern="[\d\+\-\*\/\.]*" />
+        </section>
+        <section id="calc-btn-section">
+          <button onClick={this.clearScreen}
+                  class="black-btn" id="mc" value="0">MC</button>
+          <button class="black-btn" id="mplus">M+</button>
+          <button class="black-btn" id="div-sign" value="/">&#247;</button>
+          <button onClick={this.enterEquation}
+                  class="black-btn" id="x-sign" value="*">&#215;</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="7" value="7">7</button>
+          <button class="gray-btn" id="8" value="8">8</button>
+          <button class="gray-btn" id="9" value="9">9</button>
+          <button class="gray-btn" id="minus" value="-">&#8722;</button>
+          <button class="gray-btn" id="4" value="4">4</button>
+          <button class="gray-btn" id="5" value="5">5</button>
+          <button class="gray-btn" id="6" value="6">6</button>
+          <button class="gray-btn" id="plus" value="+">&#43;</button>
+          <button class="gray-btn" id="1" value="1">1</button>
+          <button class="gray-btn" id="2" value="2">2</button>
+          <button class="gray-btn" id="3" value="3">3</button>
+          <button onClick={this.equationEquals}
+                  class="red-btn" id="equals" value="equals">&#61;</button>
+          <button class="gray-btn" id="zero" value="0">0</button>
+          <button onClick={this.enterEquation} class="gray-btn" id="decimal" value=".">.</button>
+        </section>
+      </main>
+    )
   }
 }
 
-export default App;
+export default Calculator;
