20b48838caf358206a74c398fb3aca15e46627ab
steve hanlon <rshanlon@gmail.com>
2019-02-09 16:15:52 -0800

    Hooked up enterEquation method to all numbers and equationEquals method to all operators

--- src/App.js
+++ src/App.js
@@ -41,25 +41,38 @@ class Calculator extends React.Component {
           <button onClick={this.clearScreen}
                   class="black-btn" id="mc" value="0">MC</button>
           <button class="black-btn" id="mplus">M+</button>
-          <button class="black-btn" id="div-sign" value="/">&#247;</button>
+          <button onClick={this.enterEquation}
+                  class="black-btn" id="div-sign" value="/">&#247;</button>
           <button onClick={this.enterEquation}
                   class="black-btn" id="x-sign" value="*">&#215;</button>
           <button onClick={this.enterEquation}
                   class="gray-btn" id="7" value="7">7</button>
-          <button class="gray-btn" id="8" value="8">8</button>
-          <button class="gray-btn" id="9" value="9">9</button>
-          <button class="gray-btn" id="minus" value="-">&#8722;</button>
-          <button class="gray-btn" id="4" value="4">4</button>
-          <button class="gray-btn" id="5" value="5">5</button>
-          <button class="gray-btn" id="6" value="6">6</button>
-          <button class="gray-btn" id="plus" value="+">&#43;</button>
-          <button class="gray-btn" id="1" value="1">1</button>
-          <button class="gray-btn" id="2" value="2">2</button>
-          <button class="gray-btn" id="3" value="3">3</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="8" value="8">8</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="9" value="9">9</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="minus" value="-">&#8722;</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="4" value="4">4</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="5" value="5">5</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="6" value="6">6</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="plus" value="+">&#43;</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="1" value="1">1</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="2" value="2">2</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="3" value="3">3</button>
           <button onClick={this.equationEquals}
                   class="red-btn" id="equals" value="equals">&#61;</button>
-          <button class="gray-btn" id="zero" value="0">0</button>
-          <button onClick={this.enterEquation} class="gray-btn" id="decimal" value=".">.</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="zero" value="0">0</button>
+          <button onClick={this.enterEquation}
+                  class="gray-btn" id="decimal" value=".">.</button>
         </section>
       </main>
     )
