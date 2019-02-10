dcb8ebb1e1cc4e986622b4ba35123a6ee896b38a
steve hanlon <rshanlon@gmail.com>
2019-02-10 15:37:35 -0800

    Refactor equation state from string type to array type; class to className; use .join for new equation array then eval();

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const keyboard = ["MC", "M+", "&#247;", "&#215;", "7", "8", "9", "&#8722;", "4", "5", "6", "&#43;", "1", "2", "3", "&#61;", "0", "."]
+// const keyboard = ["MC", "M+", "&#247;", "&#215;", "7", "8", "9", "&#8722;", "4", "5", "6", "&#43;", "1", "2", "3", "&#61;", "0", "."]
 
-class Calculator extends React.Component {
+class Calculator extends Component {
   state = {
     total: 0,
-    equation: "",
+    equation: [],
   }
 
   clearScreen = () => {
     this.setState({
       total: 0,
-      equation: "",
+      equation: [],
     });
   }
 
   enterEquation = (event) => {
     this.setState({
-      total: event.target.value,
-      equation: this.state.equation += event.target.value
+        total: event.target.value,
+        equation: [...this.state.equation, event.target.value]
     });
   }
 
   equationEquals = () => {
-    let solution = eval(this.state.equation);
-    this.setState({ total: solution });
+    let solution = this.state.equation.join('');
+    this.setState({ total: eval(solution) });
   }
 
   render() {
+    console.log(this.state.equation);
     return (
       <main id="calc-body">
         <section id="view-screen-container">
@@ -39,40 +40,40 @@ class Calculator extends React.Component {
         </section>
         <section id="calc-btn-section">
           <button onClick={this.clearScreen}
-                  class="black-btn" id="mc" value="0">MC</button>
-          <button class="black-btn" id="mplus">M+</button>
+                  className="black-btn" id="mc" value="0">MC</button>
+          <button className="black-btn" id="mplus">M+</button>
           <button onClick={this.enterEquation}
-                  class="black-btn" id="div-sign" value="/">&#247;</button>
+                  className="black-btn" id="div-sign" value="/">&#247;</button>
           <button onClick={this.enterEquation}
-                  class="black-btn" id="x-sign" value="*">&#215;</button>
+                  className="black-btn" id="x-sign" value="*">&#215;</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="7" value="7">7</button>
+                  className="gray-btn" id="7" value="7">7</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="8" value="8">8</button>
+                  className="gray-btn" id="8" value="8">8</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="9" value="9">9</button>
+                  className="gray-btn" id="9" value="9">9</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="minus" value="-">&#8722;</button>
+                  className="gray-btn" id="minus" value="-">&#8722;</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="4" value="4">4</button>
+                  className="gray-btn" id="4" value="4">4</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="5" value="5">5</button>
+                  className="gray-btn" id="5" value="5">5</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="6" value="6">6</button>
+                  className="gray-btn" id="6" value="6">6</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="plus" value="+">&#43;</button>
+                  className="gray-btn" id="plus" value="+">&#43;</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="1" value="1">1</button>
+                  className="gray-btn" id="1" value="1">1</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="2" value="2">2</button>
+                  className="gray-btn" id="2" value="2">2</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="3" value="3">3</button>
+                  className="gray-btn" id="3" value="3">3</button>
           <button onClick={this.equationEquals}
-                  class="red-btn" id="equals" value="equals">&#61;</button>
+                  className="red-btn" id="equals" value="equals">&#61;</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="zero" value="0">0</button>
+                  className="gray-btn" id="zero" value="0">0</button>
           <button onClick={this.enterEquation}
-                  class="gray-btn" id="decimal" value=".">.</button>
+                  className="gray-btn" id="decimal" value=".">.</button>
         </section>
       </main>
     )
